97d694f61117fbf2a33afa956bec18638450b619
dhirajprai <dhiraj.p.rai@gmail.com>
2018-11-07 10:46:19 +0530

    Add files via upload

---
+++ metaheuristics/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='metaheuristics',
+      version='0.1',
+      description='Metaheuristic Optimization Algorithms',
+      url='https://github.com//dhirajprai//metaheuristics',
+      author='Dhiraj Rai',
+      author_email='dhiraj.p.rai@gmail.com',
+      license='MIT',
+      packages=['metaheuristics'],
+      zip_safe=False)
